e407c94b6d270d062f97008545fe9b6a02df106f
Reb <rebeldomaker@icloud.com>
2026-09-12 21:14:38 +0200

    basics

---
+++ scripts/csharp/animations.cs
@@ -0,0 +1,26 @@
+/* namespace FlappyClone.scripts.csharp;
+using Godot;
+// using System;
+using System.Collections.Generic;
+
+public partial class Animations : Player // todo check the correct names for the node/meepit sprite
+	
+public class animations
+{
+	
+}
+
+public override void _Ready()
+{
+	//_template = GetNode<Label>("../../Indicators/Template");
+	//_clickButton = GetNode<TextureButton>("CenterContainer/ClickButton");
+	//_indicators = GetNode<Control>("../../Indicators");
+
+	// Sets the pivot point to the exact center of the button for scaling animations
+	//_clickButton.PivotOffset = _clickButton.Size / 2;
+
+	// Removes the white background connected to the outer border
+	// todo fix meepit sprite by removing background
+	// RemoveBackgroundContiguous(_meepitSprite);
+}
+*/
